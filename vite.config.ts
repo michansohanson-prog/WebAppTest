bc5706fbdd9c97ff76e9537c7f216af09f1f2de0
Mike <michanso.hanson@gmail.com>
2026-09-25 00:40:48 -0500

    first prep for deploy attempt - stable - edited viteconfigts and created deployyml

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  base: '/WebAppTest/', // <--- ADD THIS LINE (Replace with your actual repo name, e.g., /WebAppTest/)
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
   },
 })
